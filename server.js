b899bc1573d8817e8dbaeea4dfda7d8ecee8f27f
hesham156 <69813818+hesham156@users.noreply.github.com>
2023-10-16 11:41:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,24 +3,67 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- router.delete('/api/:id', (req, res) => {
-  // Get the ID of the user to delete
-  const id = req.params.id
+// Add a POST method for the `/site` resource
+router.post('/site', (req, res) => {
+  // Get the site data from the request body
+  const site = req.body;
 
-  // Find the user to delete
-  const user = db.users.find(user => user.id === id)
+  // Validate the site data
 
-  // If the user does not exist, return a 404 error
-  if (!user) {
-    return res.sendStatus(404)
+  // Create a new site in the database
+  db.site.push(site);
+
+  // Return the new site
+  res.json(site);
+});
+
+// Add a PUT method for the `/site/:id` resource
+router.put('/site/:id', (req, res) => {
+  // Get the ID of the site to update
+  const id = req.params.id;
+
+  // Get the updated site data from the request body
+  const site = req.body;
+
+  // Validate the site data
+
+  // Find the site to update
+  const siteToUpdate = db.site.find(site => site.id === id);
+
+  // If the site does not exist, return a 404 error
+  if (!siteToUpdate) {
+    return res.sendStatus(404);
   }
 
-  // Delete the user from the database
-  db.users.splice(db.users.indexOf(user), 1)
+  // Update the site in the database
+  siteToUpdate.name = site.name;
+  siteToUpdate.logo = site.logo;
+  siteToUpdate.width = site.width;
+  siteToUpdate.des = site.des;
 
-  // Return a 204 response indicating that the user was deleted successfully
-  res.sendStatus(204)
-})
+  // Return the updated site
+  res.json(siteToUpdate);
+});
+
+// Add a DELETE method for the `/site/:id` resource
+router.delete('/site/:id', (req, res) => {
+  // Get the ID of the site to delete
+  const id = req.params.id;
+
+  // Find the site to delete
+  const siteToDelete = db.site.find(site => site.id === id);
+
+  // If the site does not exist, return a 404 error
+  if (!siteToDelete) {
+    return res.sendStatus(404);
+  }
+
+  // Delete the site from the database
+  db.site.splice(db.site.indexOf(siteToDelete), 1);
+
+  // Return a 204 response indicating that the site was deleted successfully
+  res.sendStatus(204);
+});
 server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
